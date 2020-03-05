3da2c8c7308958643c78e29e54651ea1248d7d25
georgealexiou <george@alexiou.xyz>
2020-03-05 15:04:37 +0000

    Finished python bots, added functionality for travis integration

---
+++ burndown_bot.py
@@ -0,0 +1,84 @@
+import discord,asyncio,os
+from discord.ext import commands
+import checkTravis as travis
+
+from datetime import date
+
+bot = commands.Bot(command_prefix = '!')
+id = bot.get_guild(684446737313955871)
+
+@bot.event
+async def on_ready():
+    print('Bot is ready')
+
+
+#add new task
+@bot.command()
+async def add(ctx, task):
+    with open('tasks.txt') as f:    
+        task_list = f.read().splitlines()
+        f.close()
+
+    if task in task_list:
+        await ctx.send('Task "{}" already in the burndown chart'.format(task))
+    else:
+        await ctx.send('Task "{}" is being added to burndown chart'.format(task))
+        with open('tasks.txt', 'w') as f:
+            for task1 in task_list:
+                f.write('{}\n'.format(task1))
+
+            f.write('{}'.format(task))
+            task_list1 = f.read().splitlines()
+            f.close()
+        print (task_list1)
+
+#list all tasks
+@bot.command()
+async def tasks(ctx):
+    await ctx.send('Here are all the tasks currently on the burndown chart')
+    with open('tasks.txt') as f:    
+        task_list = f.read().splitlines()
+        f.close()
+
+    i = 1 
+    for task in task_list:
+        await ctx.send('{}. {}'.format(i, task))
+        i+=1
+
+@bot.command()
+async def burndown_help(ctx):
+    await ctx.send('!tasks -> lists all the tasks in chart')
+    await ctx.send('!log task time -> adds task to the burndown (task = task name, time = units)')
+    await ctx.send('!add -> adds new task to burndown chart')
+
+#log time for task
+@bot.command()
+async def log(ctx, task, time):
+    with open('tasks.txt') as f:    
+        task_list = f.read().splitlines()
+        f.close()
+
+    with open('burndownlog.txt') as f:    
+        log_list = f.read().splitlines()
+        f.close()
+
+
+    if (not(task in task_list)) or not(time.isdigit()):
+        await ctx.send('You made a mistake, try again')
+    else:
+        await ctx.send ('You logged {} units for task "{}"'.format(time, task))
+        with open('burndownlog.txt', 'w') as f:    
+            for log in log_list:
+                f.write('{}\n'.format(log))
+            f.write('{}.{}.{}'.format(date.today(),task,time))
+            f.close()
+
+@bot.command()
+async def status(ctx):
+    await ctx.send('Fetching info from github')
+    await ctx.send('Fetch error')
+
+with open('token.txt') as f:    
+    token = f.read().splitlines()[1]
+    f.close()
+bot.run(token)
--- travis_bot.py
+++ travis_bot.py
@@ -1,77 +1,113 @@
 import discord,asyncio,os
 from discord.ext import commands
+import checkTravis as travis
 from datetime import date
 
+from multiprocessing import Process
+
+import imaplib, email
+import time
+import datetime as dt
+from os import path
+from datetime import timezone
+
+################## DISCORD BOT #########################################3
+
+def run():
+    print ('Bot up and running')
+
 bot = commands.Bot(command_prefix = '!')
+<<<<<<< Updated upstream
+=======
+id = bot.get_guild(684446737313955871)
+channel = bot.get_channel(685057313026867210)
+>>>>>>> Stashed changes
 
 @bot.event
 async def on_ready():
     print('Bot is ready')
 
-#add new task
 @bot.command()
-async def add(ctx, task):
-    with open('tasks.txt') as f:    
-        task_list = f.read().splitlines()
-        f.close()
+async def check_travis(ctx):
+    await ctx.send('Live fetching - Active')
+    while True:
+        print(temp.ready)
+        if temp.ready is True:
+            if temp.passed:
+                await ctx.send('Your build has Passed')
+            else:
+                await ctx.send('Your build has Failed')
+            temp.ready = False
+        time.sleep(1)
 
-    if task in task_list:
-        await ctx.send('Task "{}" already in the burndown chart'.format(task))
-    else:
-        await ctx.send('Task "{}" is being added to burndown chart'.format(task))
-        with open('tasks.txt', 'w') as f:
-            for task1 in task_list:
-                f.write('{}\n'.format(task1))
-
-            f.write('{}'.format(task))
-            task_list1 = f.read().splitlines()
-            f.close()
-        print (task_list1)
-
-#list all tasks
-@bot.command()
-async def tasks(ctx):
-    await ctx.send('Here are all the tasks currently on the burndown chart')
-    with open('tasks.txt') as f:    
-        task_list = f.read().splitlines()
-        f.close()
-
-    i = 1 
-    for task in task_list:
-        await ctx.send('{}. {}'.format(i, task))
-        i+=1
-
-@bot.command()
-async def burndown_help(ctx):
-    await ctx.send('!tasks -> lists all the tasks in chart')
-    await ctx.send('!log task time -> adds task to the burndown (task = task name, time = units)')
-    await ctx.send('!add -> adds new task to burndown chart')
-
-#log time for task
-@bot.command()
-async def log(ctx, task, time):
-    with open('tasks.txt') as f:    
-        task_list = f.read().splitlines()
-        f.close()
-
-    with open('burndownlog.txt') as f:    
-        log_list = f.read().splitlines()
-        f.close()
-
-
-    if (not(task in task_list)) or not(time.isdigit()):
-        await ctx.send('You made a mistake, try again')
-    else:
-        await ctx.send ('You logged {} units for task "{}"'.format(time, task))
-        with open('burndownlog.txt', 'w') as f:    
-            for log in log_list:
-                f.write('{}\n'.format(log))
-            f.write('{}_{}_{}'.format(date.today(),task,time))
+####################### EMAIL ##########################################
+class Mail:
+    def __init__(self):
+        with open('details.txt') as f:    
+            details = f.read().splitlines()
             f.close()
 
-@bot.command()
-async def status(ctx):
-    await ctx.send('Fetching info from github')
-    await ctx.send('Fetch error')
+        self.user = details[0]
+        self.password = details[1]
+        self.imap = 'imap.gmail.com'
 
+        self.mail = imaplib.IMAP4_SSL('imap.gmail.com')
+        self.ready = False
+        self.passed = False
+
+    def check(self):
+        self.mail.login(self.user,self.password)
+        while True:
+            self.mail.select('INBOX')
+            unread = self.mail.search(None,'UnSeen')[1][0]
+            if (unread == b''):
+                print('Nothing new')
+            else:
+                result,data = self.mail.fetch(unread,'(RFC822)')
+                raw = email.message_from_bytes(data[0][1])
+                print(str(self.get_body(raw)))
+                string_message = str(self.get_body(raw))
+
+                if 'Software-Engineering-Group-Project' in string_message:
+                    self.parse_email(str(self.get_body(raw)))
+
+            time.sleep(5)
+
+    def parse_email(self,contents):
+        status = ''
+        if 'errored' in contents:
+            print('errored')
+            self.ready = True
+            self.passed = False
+        elif 'passed' in contents:
+            print('passed')
+            self.ready = True
+            self.passed = False
+
+    def get_body(self,msg):
+        if msg.is_multipart():
+            return self.get_body(msg.get_payload(0))
+        else:
+            return msg.get_payload(None,True)
+        
+####################### THREADING ########################################
+def run_bot():
+    with open('token.txt') as f:    
+        token = f.read().splitlines()[0]
+        f.close()
+    bot.run(token)
+
+def run_mail():
+    temp.check()
+
+temp = Mail()
+
+<<<<<<< Updated upstream
 bot.run('')
+=======
+if __name__ == '__main__':
+    p1 = Process(target = run_bot)
+    p2 = Process(target = run_mail)
+    p1.start()
+    p2.start()
+>>>>>>> Stashed changes
